952cde6470fc9f830263ab178763efa91a9be8f5
Redeamer <wehaagmans@gmail.com>
2023-04-20 23:06:04 +0200

    refactor: :recycle: Refactor the naming of the modal reference variable.

--- src/modal/exampleModal/exampleResponse.ts
+++ src/modal/exampleModal/exampleResponse.ts
@@ -3,7 +3,7 @@ import { ModalSubmitInteraction } from 'discord.js';
 
 const exampleResponse: Modal = {
 	// Name should be the same as the customID in the modal's set at the modals creation
-	name: 'exampleResponse',
+	modalReference: 'exampleResponse',
 	run: async (interaction: ModalSubmitInteraction) => {
 		const text = interaction.fields.getTextInputValue('textInput');
 		interaction.reply({
